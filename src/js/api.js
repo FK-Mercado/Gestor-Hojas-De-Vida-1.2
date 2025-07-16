c790f3faced62c117726faa2ae72555d80d31011
Martin Escobar Mercado <martinescob36@gmail.com>
2025-07-16 16:37:21 -0500

    feat: creation of the api/crud

--- src/js/api.js
+++ src/js/api.js
@@ -1,10 +1,48 @@
-export async function apis() {
+const API_URL = "http://localhost:3000";
 
-    const endpoint = 'http://localhost:3000'
+export const api = {
+    //GET Method
+    async getAdmin(resource) {
+        const res = await fetch(`${API_URL}/${resource}`);
+        if (!res.ok) throw new Error(`GET ${resource} failed`);
+        return res.json();
+    },
 
-    getAdmin = endpoint
+    async getCoder(resource, id){
+        const res = await fetch(`${API_URL}/${resource}/${id}`);
+        if (!res.ok) throw new Error(`GET ${resource} failed`);
+        return res.json();
+    },
 
-    getCoder = endpoint
+    //POST Method
+    async post(resource, data) {
+        const res = await fetch(`${API_URL}/${resource}`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(data),
+        });
+        if (!res.ok) throw new Error(`POST ${resource} failed`);
+        return res.json();
+    },  
 
-    
-}
+    //PUT Method
+    async put(resource, id, data) {
+        const res = await fetch(`${API_URL}/${resource}/${id}`, {
+            method: "PUT",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(data),
+        });
+        if (!res.ok) throw new Error(`PUT ${resource}/${id} failed`);
+        return res.json();
+    },
+
+    //DELETE Method
+    async delete(resource, id) {
+        const res = await fetch(`${API_URL}/${resource}/${id}`, {
+            method: "DELETE",
+        });
+        if (!res.ok) throw new Error(`DELETE ${resource}/${id} failed`);
+        return true;
+    },
+
+};
